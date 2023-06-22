23900e98580b8e535c7ec657ad152982880a30fc
fanifrancs <fanikufran6@gmail.com>
2023-06-22 02:35:38 +0100

    update

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # fantastic-waddle
-This repo is basically a Python script that implements an anonymous messaging app. The app interacts with a MongoDB database to store user data and messages. Authentication is handled using bcrypt.
+Basically a Python script that implements an anonymous messaging app. The program interacts with a MongoDB database to store user data and messages. Authentication is handled using bcrypt, a secure password hashing function.
 
 ## Setup
+You need to have installed
+- Python
+- MongoDB
+
+[Click here]() to install MongoDB locally if you intend to use it as part of your development environment, or [click here]() to setup a cloud database. Setting up the database won't be a problem if you already know what you are doing. Clone this repo onto your machine using
+```
+git clone https://github.com/fanifrancs/fantastic-waddle.git
+```
+Navigate to repo directory, open up the terminal and run
+```
+pip install -r requirements.txt
+```
+to install dependencies. Make sure MongoDB is running and ready to accept connections. Then run `app.py` to start app and hopefully, everything should work fine.
--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-import pymongo, bcrypt, datetime
+import pymongo, bcrypt, datetime, pwinput
 
 logged_in = False
-current_user = None
-# A variable always holding all user data : ) awkward, I know
-users = None
+# user variable holds user documents from collection
+current_user = users = None
 
 def invalid_username(username):
   username = username.replace(' ', '')
@@ -18,6 +17,7 @@ def collect_command():
     execute_command(command)
 
 def login():
+    # session variables to be reassigned
     global logged_in, current_user
 
     if current_user or logged_in:
@@ -25,24 +25,25 @@ def login():
         collect_command()
 
     username = str(input('Enter username: '))
-
     user = users.find_one({'username': username})
-    if user is None:
-        print('User does not exist in database.')
-        login()
 
-    password = str(input('Enter password: '))
+    if user is None:
+        print('User not found.')
+        collect_command()
+
+    password = pwinput.pwinput(prompt='Enter password: ')
     password = password.encode('utf-8')
 
     user = users.find_one({'username': username})
-
     if not bcrypt.checkpw(password, user['password']):
         print('Invalid username or password')
-        login()
+        collect_command()
 
+    # else login and set session variables
     logged_in = True
     current_user = user['username']
-    print('You are logged in as ' , current_user)
+
+    print('You are logged in as' , current_user)
     collect_command()
 
 def register():
@@ -51,32 +52,29 @@ def register():
         collect_command()
 
     username = str(input('Enter username: '))
-
     if invalid_username(username):
         print('Username is not valid')
-        register()
+        collect_command()
 
     username = sanitize_username(username)
-    print('Your username is ' + username)
+    print('Your username is', username)
 
-    password = str(input('Enter password: '))
-
+    password = pwinput.pwinput(prompt='Enter password: ')
     if len(password) < 6:
         print('Password must be at least 6 characters long.')
-        password = str(input('Enter password: '))
+        collect_command()
 
-    password2 = str(input('Confirm password: '))
-
+    password2 = pwinput.pwinput(prompt='Confirm password: ')
     if password != password2:
         print('Passwords do not match.')
-        register()
+        collect_command()
 
     password = password.encode('utf-8')
 
     if users.find_one({'username': username}) is not None:
         print('A user with the given username is already registered.')
-        register()
-
+        collect_command()
+    
     # else register user
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(password, salt)
@@ -92,12 +90,12 @@ def register():
 
 def message():
     username = str(input('Enter username: '))
-
     user = users.find_one({'username': username})
+
     if user is None:
-        print('User does not exist in database.')
+        print('User not found.')
         collect_command()
-        
+
     text = str(input('Enter message: '))
     message = {
         'date': datetime.datetime.now(),
@@ -105,8 +103,8 @@ def message():
     }
 
     user['messages'].append(message)
-
     users.update_one({'_id': user['_id']}, {'$set': user})
+
     print('Message sent!')
     collect_command()
 
@@ -114,24 +112,31 @@ def messages():
     if not (current_user or logged_in):
         print('Please login first')
         collect_command()
-    user = users.find_one({'username': current_user})
 
-    for message in user['messages']:
-        print(f"{message['date']}: {message['message']}")
-    
+    user = users.find_one({'username': current_user})
+    messages = user['messages']
+
+    if len(messages) == 0:
+        print('You have no message.')
+    else:
+        for message in messages:
+            print(f"{message['date']}: {message['message']}")
+
     collect_command()
 
 def contact():
-    print('Send an email to fanikufran6@gmail.com')  
+    print('Send an email to fanikufran6@gmail.com')
+    collect_command()
 
 def logout():
     global current_user, logged_in
+
     if current_user or logged_in:
-        current_user = None
-        logged_in = None
+        current_user = logged_in = None
         print('You are logged out')
     else:
         print('You are not logged in')
+
     collect_command()
 
 def execute_command(command):
@@ -149,17 +154,23 @@ def execute_command(command):
     elif command == 'logout':
         logout()
     else:
-        print('Invalid command')
+        print('Invalid command.')
         collect_command()
 
 def connect_db():
+    '''
+    if you are using a cloud database, then the mongodb connection url
+    will be different. Replace your_database with your database name and
+    then if all goes well, you have a users collection ready to store data.
+    '''
     try:
         global users
         client = pymongo.MongoClient('mongodb://localhost:27017')
-        db = client['py_anonimaze']
+        db = client['fantastic_waddle']
         users = db['users']
+        print('Welcome.\nCommands: login, register, message, messages, contact, logout')
+        collect_command()
     except Exception as e:
         print(e)
 
 connect_db()
-collect_command()
